2e8f32b92d884d704265cca9b21287a25823fcbb
sunilrav <rsunilbabu@gmail.com>
2013-06-14 16:11:24 -0600

    Added ninject

---
+++ Wishlist/Wishlist.Service/ServiceNinjectModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ninject.Modules;
+
+namespace Wishlist.Service
+{
+    class ServiceNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
